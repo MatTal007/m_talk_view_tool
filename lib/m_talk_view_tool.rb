d6399ec383534800662585ddfdda1676bc03dc19
Mateusz Talkowski <fly_master@onet.eu>
2021-12-16 13:54:00 +0100

    MTalk tool initial commit

---
+++ lib/m_talk_view_tool.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "m_talk_view_tool/version"
+
+module MTalkViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/m_talk_view_tool/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MTalkViewTool
+  VERSION = "0.1.0"
+end
